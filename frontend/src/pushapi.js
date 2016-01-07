fb7e595072bb3bc2a9eb29c96d52c959e63a7473
mlesikov <mlesikov@gmail.com>
2016-01-07 17:11:44 +0200

    tests without tagsdsadasd

--- frontend/src/pushapi.js
+++ frontend/src/pushapi.js
@@ -22,7 +22,7 @@ angular.module('clouway-push', [])
 
     /**
      * Some test changes
-     * Set a method to call for opening of connection.
+     * Set a method to call for opening of connection. dasdas
      * The method must return a promise.
      *
      * @param {function(subscriber)} method method that will be called for opening of connection.
