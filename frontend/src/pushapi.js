8fdb9debf05dd0137b132ba3e64ab3cbc4c7143e
mlesikov <mlesikov@gmail.com>
2016-01-07 14:48:18 +0200

    another change for 3.4.0

--- frontend/src/pushapi.js
+++ frontend/src/pushapi.js
@@ -23,7 +23,7 @@ angular.module('clouway-push', [])
     /**
      * Some test changes
      * Set a method to call for opening of connection.
-     * The method must return a promise.          1223423432313221
+     * The method must return a promise.          122342343231wqewewq3221
      *
      * @param {function(subscriber)} method method that will be called for opening of connection.
      * @returns {*} the provider instance for chaining purposes.
